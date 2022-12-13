ba334236c1d1da534caf600b832fd3f6e3db9104
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-13 23:08:32 +0530

    Docker file changes

--- webpage/deployment/deploy.sh
+++ webpage/deployment/deploy.sh
@@ -17,4 +17,4 @@ sudo /etc/init.d/nginx start || sudo /etc/init.d/nginx start
 #nohup python manage.py zwift_bot &
 #python manage.py whois_bot_start &
 # Running Server
-uwsgi --socket mysite.sock --module webpage.webpage.wsgi --buffer-size=100000 --chmod-socket=666 --master --processes 4 --threads 2
+uwsgi --socket mysite.sock --module webpage.wsgi --buffer-size=100000 --chmod-socket=666 --master --processes 4 --threads 2
