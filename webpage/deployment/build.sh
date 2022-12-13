c95bfee67cfdb14d80d5bae3d11b419fb5b79740
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-13 22:45:00 +0530

    Docker file changes

--- webpage/deployment/build.sh
+++ webpage/deployment/build.sh
@@ -3,7 +3,7 @@
 docker build -t the-game-web-image:prd -f Dockerfile .
 if [ `echo $?` == 0 ]; then
 	docker rm -f the-game-web
-	docker run -dt --restart=always -p 8010:8010 --name the-game-web the-game-web-image:prd
+	docker run -dt --restart=always -p 8011:8011 --name the-game-web the-game-web-image:prd
 fi
 
 
