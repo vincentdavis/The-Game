2c9f66d711468fc0bed8de9e564fe8ee0550214b
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-13 22:43:53 +0530

    Removed build

--- webpage/deployment/build.sh
+++ webpage/deployment/build.sh
@@ -2,7 +2,7 @@
 
 docker build -t the-game-web-image:prd -f Dockerfile .
 if [ `echo $?` == 0 ]; then
-	docker rm -f frr
+	docker rm -f the-game-web
 	docker run -dt --restart=always -p 8010:8010 --name the-game-web the-game-web-image:prd
 fi
 
