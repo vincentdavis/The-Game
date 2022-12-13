24e1667f9c0ce5171af86aa33366087766267f6e
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-13 22:51:27 +0530

    Docker file changes

--- webpage/deployment/deploy.sh
+++ webpage/deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/we-race-here/The-Game.git /home/jenkins/the_game
-cd /home/jenkins/the_game
+cd /home/jenkins/the_game/webpage/
 git checkout master
 mkdir -p media
 
@@ -18,4 +18,4 @@ sudo /etc/init.d/nginx start || sudo /etc/init.d/nginx start
 #nohup python manage.py zwift_bot &
 #python manage.py whois_bot_start &
 # Running Server
-uwsgi --socket mysite.sock --module webpage.webpage.wsgi --buffer-size=100000 --chmod-socket=666 --master --processes 4 --threads 2
+uwsgi --socket mysite.sock --module webpage.wsgi --buffer-size=100000 --chmod-socket=666 --master --processes 4 --threads 2
